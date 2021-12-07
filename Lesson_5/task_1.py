c9498a93b59f4560e09262340cfcfc870ec193dd
Denis <60378291+KapaturinD@users.noreply.github.com>
2021-12-08 02:47:20 +0600

    Add files via upload

---
+++ Lesson_5/task_1.py
@@ -0,0 +1,6 @@
+with open('task_1.txt', 'w', encoding='utf-8') as f:
+    while True:
+        line = input('Введите новую строку -')
+        if not line:
+            break
+        f.write(f'{line}\n')
